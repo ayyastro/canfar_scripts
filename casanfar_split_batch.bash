67a1341604d87e8d45e10411787068509ffe98e9
e-koch <koch.eric.w@gmail.com>
2015-03-02 13:49:42 -0700

    More updates to split script.

--- casanfar_split_batch.bash
+++ casanfar_split_batch.bash
@@ -3,18 +3,21 @@ echo 'Making dirs'
 mkdir -p ${TMPDIR}/{vos,vos_cache,proc,vos_link}
 echo 'Mount VOS in readonly mode'
 
+# Source bash profile
+source .bash_profile
+
 # Clone CANFAR repo
 git clone https://github.com/e-koch/canfar_scripts.git
 
 sudo mountvofs --vospace vos:MWSynthesis/VLA/14B-088_20141211_1418355329562 --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache --readonly
 echo 'Run casapy'
 cd ${TMPDIR}/proc
-casapy -c /home/ekoch/canfar_scripts/casanfar_split.py
+/home/ekochcasa-stable-4.4.95/casapy --nogui -c /home/ekoch/canfar_scripts/casanfar_split.py
 echo 'Unmount VOS'
 sudo fusermount -u ${TMPDIR}/vos
 echo 'Mount VOS'
 sudo mountvofs --vospace vos:vos:MWSynthesis/VLA/14B-088_20141211_1418355329562/ --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache
 echo 'Copy files to VOS'
-cp -rf ${TMPDIR}/proc/* ${TMPDIR}/vos/
+sudo cp -rf ${TMPDIR}/proc/* ${TMPDIR}/vos/
 echo 'Unmount VOS'
 sudo fusermount -u ${TMPDIR}/vos
