5b62120426d8d24a05481895d240e5eb5fec361e
e-koch <koch.eric.w@gmail.com>
2015-03-02 12:21:50 -0700

    Use temp directories.

--- casanfar_split_batch.bash
+++ casanfar_split_batch.bash
@@ -1,20 +1,20 @@
 #!/bin/bash
 echo 'Making dirs'
-mkdir -p /staging/tmp/{vos,vos_cache,proc}
+mkdir -p ${TMPDIR}/{vos,vos_cache,proc,vos_link}
 echo 'Mount VOS in readonly mode'
 
 # Clone CANFAR repo
 git clone https://github.com/e-koch/canfar_scripts.git
 
-sudo mountvofs --vospace vos:MWSynthesis/VLA/14B-088_20141211_1418355329562 --mountpoint /staging/tmp/vos --cache_dir /staging/tmp/vos_cache --readonly
+sudo mountvofs --vospace vos:MWSynthesis/VLA/14B-088_20141211_1418355329562 --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache --readonly
 echo 'Run casapy'
-cd /staging/tmp/proc
+cd ${TMPDIR}/proc
 casapy -c /home/ekoch/canfar_scripts/casanfar_split.py
 echo 'Unmount VOS'
-sudo fusermount -u /staging/tmp/vos
+sudo fusermount -u ${TMPDIR}/vos
 echo 'Mount VOS'
-sudo mountvofs --vospace vos:vos:MWSynthesis/VLA/14B-088_20141211_1418355329562/ --mountpoint /staging/tmp/vos --cache_dir /staging/tmp/vos_cache
+sudo mountvofs --vospace vos:vos:MWSynthesis/VLA/14B-088_20141211_1418355329562/ --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache
 echo 'Copy files to VOS'
-cp -rf /staging/tmp/proc/* /staging/tmp/vos/
+cp -rf ${TMPDIR}/proc/* ${TMPDIR}/vos/
 echo 'Unmount VOS'
-sudo fusermount -u /staging/tmp/vos
+sudo fusermount -u ${TMPDIR}/vos
