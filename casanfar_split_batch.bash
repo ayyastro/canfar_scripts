6fda264eca193e898000c38af7f51e6677e8518e
e-koch <koch.eric.w@gmail.com>
2015-03-02 15:12:02 -0700

    Point to home directory

--- casanfar_split_batch.bash
+++ casanfar_split_batch.bash
@@ -4,15 +4,15 @@ mkdir -p ${TMPDIR}/{vos,vos_cache,proc,vos_link}
 echo 'Mount VOS in readonly mode'
 
 # Source bash profile
-source .bash_profile
+source /home/ekoch/.bash_profile
 
 # Clone CANFAR repo
-git clone https://github.com/e-koch/canfar_scripts.git
+git clone https://github.com/e-koch/canfar_scripts.git /home/ekoch/
 
 sudo mountvofs --vospace vos:MWSynthesis/VLA/14B-088_20141211_1418355329562 --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache --readonly
 echo 'Run casapy'
 cd ${TMPDIR}/proc
-/home/ekochcasa-stable-4.4.95/casapy --nogui -c /home/ekoch/canfar_scripts/casanfar_split.py
+/home/ekoch/casa-stable-4.4.95/casapy --nogui -c /home/ekoch/canfar_scripts/casanfar_split.py
 echo 'Unmount VOS'
 sudo fusermount -u ${TMPDIR}/vos
 echo 'Mount VOS'
