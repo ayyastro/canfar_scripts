572989350301b9a4ccc72a761d58641c53f1feeb
e-koch <koch.eric.w@gmail.com>
2015-03-02 10:04:43 -0700

    Split HI spws.

---
+++ casanfar_split.py
@@ -0,0 +1,96 @@
+import os
+import numpy as np
+from scipy.optimize import curve_fit
+
+
+def gauss(x, A, mu, sigma):
+    return A*np.exp(-(x-mu)**2/(2.*sigma**2))
+
+
+
+scriptmode = True
+
+SDM_name = '14B-088.sb30023144.eb30070731.57002.919034293984' # The prefix to use for all output files
+
+# Set up some useful variables (these will be altered later on)
+msfile = SDM_name + '.ms'
+hisplitms = SDM_name + '.hi.ms'
+splitms = SDM_name + '.hi.src.split.ms'
+
+pathname=os.environ.get('CASAPATH').split()[0]
+pipepath = '/home/ekoch/pipe_scripts/'
+#pipepath = '/home/dario/pipe_scripts/'
+
+source = 'M33'
+
+# VOS stuff
+vos_dir = '../vos/'
+vos_proc = './'
+
+#%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%%&%&%&%&%&%&%%&%
+# Find the 21cm spw and check if the obs
+# is single pointing or mosaic
+#%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%%&%&%&%&%&%&%%&%
+
+print "Find HI spw..."
+
+# But first find the spw corresponding to it
+tb.open(vos_dir+msfile+'/SPECTRAL_WINDOW')
+freqs = tb.getcol('REF_FREQUENCY')
+nchans = tb.getcol('NUM_CHAN')
+tb.close()
+
+spws = range(0, len(freqs))
+
+
+# Select the 21cm
+
+sel = np.where((freqs > 1.40*10**9) & (freqs < 1.43*10**9))
+hispw = str(spws[sel[0][0]])
+freq = freqs[sel[0][0]]
+nchan = nchans[sel[0][0]]
+
+print "Selected spw ", hispw, "with frequency ", freq, "and ", nchan, " channels"
+print "Starting split the HI line"
+
+# Mosaic or single pointing?
+
+tb.open(vos_dir+msfile+'/FIELD')
+names = tb.getcol('NAME')
+tb.close()
+
+moscount = 0
+
+for name in names:
+    chsrc = name.find(source)
+
+    if chsrc != -1:
+        moscount = moscount+1
+
+if moscount > 1:
+    imagermode = "mosaic"
+else:
+    imagermode = "csclean"
+
+
+#%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%
+# Split the corrected source data from the rest
+#%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%&%
+
+
+print "Starting source split..."
+
+#os.system('md5sum $(find '+vos_dir+hisplitms+') > '+vos_proc+hisplitms+'.md5')
+
+os.system('rm -rf '+vos_proc+splitms)
+
+default('split')
+vis = vos_dir+hisplitms
+outputvis = vos_proc+splitms
+field = source
+spw = hispw
+datacolumn = 'corrected'
+keepflags = False
+split()
+
+print "Created splitted-source .ms "+splitms
---
+++ casanfar_split_batch.bash
@@ -0,0 +1,20 @@
+#!/bin/bash
+echo 'Making dirs'
+mkdir -p /staging/tmp/{vos,vos_cache,proc}
+echo Mount VOS in readonly mode
+
+# Clone CANFAR repo
+git clone https://github.com/e-koch/canfar_scripts.git
+
+sudo mountvofs --vospace vos:MWSynthesis/VLA/14B-088_20141211_1418355329562 --mountpoint /staging/tmp/vos --cache_dir /staging/tmp/vos_cache --readonly
+echo 'Run casapy'
+cd /staging/tmp/proc
+casapy -c /home/ekoch/canfar_scripts/casanfar_split.py
+echo 'Unmount VOS'
+sudo fusermount -u /staging/tmp/vos
+echo 'Mount VOS'
+sudo mountvofs --vospace vos:vos:MWSynthesis/VLA/14B-088_20141211_1418355329562/ --mountpoint /staging/tmp/vos --cache_dir /staging/tmp/vos_cache
+echo 'Copy files to VOS'
+cp -rf /staging/tmp/proc/* /staging/tmp/vos/
+echo 'Unmount VOS'
+sudo fusermount -u /staging/tmp/vos
