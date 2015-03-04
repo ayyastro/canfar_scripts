8c9ceef1c9580bda54328eead6cb8e3774a4bdf2
e-koch <koch.eric.w@gmail.com>
2015-03-03 20:00:08 -0700

    Add pipeline scripts.

---
+++ cal_pipe/update_pipeline_paths.py
@@ -0,0 +1,34 @@
+
+'''
+Update EVLA pipeline variables to the current system.
+'''
+
+
+def update_paths(pipe_dict, ms_path, pipepath):
+
+    pipe_dict['ms_active'] = ms_path
+    pipe_dict['SDM_name'] = ms_path+".ms"
+    pipe_dict['pipepath'] = pipepath
+
+    return pipe_dict
+
+
+if __name__ == '__main__':
+
+    import sys
+
+    pipe_var_file = str(sys.argv[1])
+
+    ms_path = str(sys.argv[2])
+
+    pipepath = str(sys.argv[3])
+
+    import shelve
+
+    pipe_dict = shelve.open(pipe_var_file, writeback=True)
+
+    pipe_dict = update_paths(pipe_dict, ms_path, pipepath)
+
+    pipe_dict.flush()
+
+    pipe_dict.close()
