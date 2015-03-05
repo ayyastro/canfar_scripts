ec6a1ec7335ea311eb474fe81adb3878b552d58e
e-koch <koch.eric.w@gmail.com>
2015-03-05 09:16:31 -0700

    Missing part of path to the ms

--- split/casanfar_split_batch.bash
+++ split/casanfar_split_batch.bash
@@ -13,14 +13,14 @@ echo 'Mount VOS in readonly mode'
 # Clone CANFAR repo
 git clone https://github.com/e-koch/canfar_scripts.git /home/ekoch/canfar_scripts
 
-mountvofs --vospace vos:MWSynthesis/VLA/14B-088_20141211_1418355329562 --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache --readonly
+mountvofs --vospace vos:MWSynthesis/VLA/14B-088_20141211_1418355329562/products/ --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache --readonly
 echo 'Run casapy'
 cd ${TMPDIR}/proc
 /home/ekoch/casa-stable-4.4.95/casapy --nogui -c /home/ekoch/canfar_scripts/split/casanfar_split.py
 echo 'Unmount VOS'
 fusermount -u ${TMPDIR}/vos
 echo 'Mount VOS'
-mountvofs --vospace vos:vos:MWSynthesis/VLA/14B-088_20141211_1418355329562/ --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache
+mountvofs --vospace vos:vos:MWSynthesis/VLA/14B-088_20141211_1418355329562/products/ --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache
 echo 'Copy files to VOS'
 cp -a ${TMPDIR}/proc/* ${TMPDIR}/vos/
 echo 'Unmount VOS'
