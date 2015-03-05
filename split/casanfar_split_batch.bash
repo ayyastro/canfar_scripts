76f7e79f972c94b60f6e88648a9a80df3baf0cd6
e-koch <koch.eric.w@gmail.com>
2015-03-05 14:34:30 -0700

    More path fixes.

--- split/casanfar_split_batch.bash
+++ split/casanfar_split_batch.bash
@@ -28,7 +28,7 @@ sudo /home/ekoch/casa-stable-4.4.95/casapy --nogui -c /home/ekoch/canfar_scripts
 echo 'Unmount VOS'
 sudo fusermount -u ${TMPDIR}/vos
 echo 'Mount VOS'
-sudo mountvofs --vospace vos:vos:MWSynthesis/VLA/14B-088/14B-088_20141211_1418355329562/products/ --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache
+sudo mountvofs --vospace vos:MWSynthesis/VLA/14B-088/14B-088_20141211_1418355329562/products/ --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache
 echo 'Copy files to VOS'
 sudo cp -a ${TMPDIR}/proc/* ${TMPDIR}/vos/
 echo 'Unmount VOS'
