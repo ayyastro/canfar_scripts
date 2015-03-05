b58bd96f6e720616f405ed53574ead9897679868
e-koch <koch.eric.w@gmail.com>
2015-03-05 12:34:38 -0700

    Try sudo on casa

--- split/casanfar_split_batch.bash
+++ split/casanfar_split_batch.bash
@@ -21,7 +21,7 @@ git clone https://github.com/e-koch/canfar_scripts.git /home/ekoch/canfar_script
 sudo mountvofs --vospace vos:MWSynthesis/VLA/14B-088_20141211_1418355329562/products/ --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache --readonly
 echo 'Run casapy'
 cd ${TMPDIR}/proc
-/home/ekoch/casa-stable-4.4.95/casapy --nogui -c /home/ekoch/canfar_scripts/split/casanfar_split.py
+sudo /home/ekoch/casa-stable-4.4.95/casapy --nogui -c /home/ekoch/canfar_scripts/split/casanfar_split.py
 echo 'Unmount VOS'
 sudo fusermount -u ${TMPDIR}/vos
 echo 'Mount VOS'
