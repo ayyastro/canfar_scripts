788dd0478e778cd4906022fd2dd7ae8edf5f2e2c
e-koch <koch.eric.w@gmail.com>
2015-03-13 13:29:25 -0600

    Split clean-up; accepts folder as argument

--- split/casanfar_split_batch.bash
+++ split/casanfar_split_batch.bash
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+###########################
+# {1} is path to MS in VOS
+# {2} is name of MS
+###########################
+
 # Source bash profile
 source /home/ekoch/.bash_profile
 source /home/ekoch/.bashrc
@@ -11,29 +16,25 @@ export USER='ekoch'
 getCert
 
 echo 'Making dirs'
-chmod 777 ${TMPDIR}
-mkdir -p -m 777 ${TMPDIR}/{vos,vos_cache,proc,vos_link}
-chown -R ${USER} ${TMPDIR}/{vos,vos_cache,proc,vos_link}
-echo 'Mount VOS in readonly mode'
+mkdir -p ${TMPDIR}/{vos,vos_cache,proc,vos_link}
 
 # Clone CANFAR repo
 git clone https://github.com/e-koch/canfar_scripts.git /home/ekoch/canfar_scripts
 
-sudo mountvofs --vospace vos:MWSynthesis/VLA/14B-088/14B-088_20141211_1418355329562/products/ --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache --readonly
 
-sudo ls -al ${TMPDIR}/vos
+echo 'Mount VOS in readonly mode'
+mountvofs --vospace vos:MWSynthesis/VLA/14B-088/${1}/products/ --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache --readonly
+
+cd ${TMPDIR}/proc
 
 echo 'Run casapy'
-cd ${TMPDIR}/proc
-sudo /home/ekoch/casa-stable-4.4.95/casapy --nogui -c /home/ekoch/canfar_scripts/split/casanfar_split.py
+casapy --nogui -c /home/ekoch/canfar_scripts/split/casanfar_split.py {2}
 
-ls ${TMPDIR}/proc/
-sudo ls -al ${TMPDIR}/vos
 
 echo 'Unmount VOS'
 sudo fusermount -u ${TMPDIR}/vos
 echo 'Mount VOS'
-sudo mountvofs --vospace vos:MWSynthesis/VLA/14B-088/14B-088_20141211_1418355329562/products/ --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache
+mountvofs --vospace vos:MWSynthesis/VLA/14B-088/${1}/products/ --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache
 echo 'Copy files to VOS'
 sudo cp -a ${TMPDIR}/proc/* ${TMPDIR}/vos/
 echo 'Unmount VOS'
