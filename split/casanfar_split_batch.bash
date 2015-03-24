78066286a4ee1d1b6e28eec654346241f42682b5
e-koch <koch.eric.w@gmail.com>
2015-03-24 10:09:04 -0600

    Decide where to send the outputted split MS

--- split/casanfar_split_batch.bash
+++ split/casanfar_split_batch.bash
@@ -12,9 +12,6 @@ source /home/ekoch/.bash_profile
 # Set username. Otherwise CASA crashes.
 export USER='ekoch'
 
-# Get certificate
-getCert
-
 echo 'Making dirs'
 mkdir -p ${TMPDIR}/{vos,vos_cache,proc,vos_link}
 
@@ -36,9 +33,26 @@ casapy --nogui -c /home/ekoch/canfar_scripts/split/casanfar_split.py ${1}/produc
 
 
 sudo fusermount -u ${TMPDIR}/vos
-echo 'Mount VOS'
-mountvofs --vospace vos:MWSynthesis/VLA/14B-088/${1}/products/ --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache
-echo 'Copy files to VOS'
-cp -a ${TMPDIR}/proc/* ${TMPDIR}/vos/
-echo 'Unmount VOS'
-fusermount -u ${TMPDIR}/vos
+
+# Where to send the split file
+to_vos=0
+
+if [ $to_vos ]
+    then
+
+    # Get certificate
+    getCert
+    echo 'Mount VOS'
+    mountvofs --vospace vos:MWSynthesis/VLA/14B-088/${1}/products/ --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache
+    echo 'Copy files to VOS'
+    cp -a ${TMPDIR}/proc/* ${TMPDIR}/vos/
+    echo 'Unmount VOS'
+    fusermount -u ${TMPDIR}/vos
+
+else
+    mount_data_write
+
+    echo "Copying files"
+    cp -a ${TMPDIR}/proc/* ${TMPDIR}/vos/
+    echo "Unmounting"
+    sudo fusermount -u ${TMPDIR}/vos/
