88ec18e398398005fac9d95c9a8ad2c00067d317
e-koch <koch.eric.w@gmail.com>
2015-03-27 16:14:29 -0600

    Locally save SPW plots from VOS

---
+++ cp_vos_plots.bash
@@ -0,0 +1,27 @@
+
+# Grab plot off of VOS and save locally, which is the given argument.
+echo ${1}
+
+vcp vos:MWSynthesis/VLA/14B-088/14B-088_20141116_1416220528659/products/spw_plots/*.png ${1}/14B-088_20141116_1416220528659/
+
+vcp vos:MWSynthesis/VLA/14B-088/14B-088_20141211_1418355329562/products/spw_plots/*.png ${1}/14B-088_20141211_1418355329562/
+
+vcp vos:MWSynthesis/VLA/14B-088/14B-088_20141115_1416131727330/products/spw_plots/*.png ${1}/14B-088_20141115_1416131727330/
+
+vcp vos:MWSynthesis/VLA/14B-088/14B-088_20141115_1416114929005/products/spw_plots/*.png ${1}/14B-088_20141115_1416114929005/
+
+vcp vos:MWSynthesis/VLA/14B-088/14B-088_20141116_1416201340117/products/spw_plots/*.png ${1}/14B-088_20141116_1416201340117/
+
+vcp vos:MWSynthesis/VLA/14B-088/14B-088_20141110_1415682929669/products/spw_plots/*.png ${1}/14B-088_20141110_1415682929669/
+
+vcp vos:MWSynthesis/VLA/14B-088/14B-088_20141113_1415960127723/products/spw_plots/*.png ${1}/14B-088_20141113_1415960127723/
+
+vcp vos:MWSynthesis/VLA/14B-088/14B-088_20141021_1413960928386/products/spw_plots/*.png ${1}/14B-088_20141021_1413960928386/
+
+vcp vos:MWSynthesis/VLA/14B-088/14B-088_20141121_1416632129289/products/spw_plots/*.png ${1}/14B-088_20141121_1416632129289/
+
+vcp vos:MWSynthesis/VLA/14B-088/14B-088_20141124_1416908128916/products/spw_plots/*.png ${1}/14B-088_20141124_1416908128916/
+
+vcp vos:MWSynthesis/VLA/14B-088/14B-088_20141114_1416028534377/products/spw_plots/*.png ${1}/14B-088_20141114_1416028534377/
+
+vcp vos:MWSynthesis/VLA/14B-088/14B-088_20141114_1416081448347/products/spw_plots/*.png ${1}/14B-088_20141114_1416081448347/
