2c4b0fc052ffd2118fc153e38c3df0140c93ae73
e-koch <koch.eric.w@gmail.com>
2015-03-29 12:54:21 -0600

    if/else for different data mounts

--- img_pipe/archival_data/canfar_AT0206_img.bash
+++ img_pipe/archival_data/canfar_AT0206_img.bash
@@ -7,6 +7,9 @@ source /home/ekoch/.bash_profile
 # Set username. Otherwise CASA crashes.
 export USER='ekoch'
 
+# Where to get the data from
+use_vos=true
+
 # Get certificate
 getCert
 
@@ -16,26 +19,53 @@ rm -rf /home/ekoch/canfar_scripts
 git clone https://github.com/e-koch/canfar_scripts.git /home/ekoch/canfar_scripts
 
 echo "Mounting data"
-mount_data_write
+if [ $use_vos==true ]
+    then
+    mountvofs --vospace vos:MWSynthesis/ --mountpoint ${TMPDIR}/vos --cache_dir ${TMPDIR}/vos_cache
+else
+    mount_data_write
+fi
 
 echo "Copying files onto VM"
 
-cp ${TMPDIR}/vos/M33_mask.image.zip ${TMPDIR}/proc
-echo "Done M33_mask"
-ls -al ${TMPDIR}/proc/
+if [ $use_vos==true ]
+    then
+    cp ${TMPDIR}/vos/Arecibo/M33_mask.image.zip ${TMPDIR}/proc
+    echo "Done M33_mask"
+    ls -al ${TMPDIR}/proc/
 
-cp ${TMPDIR}/vos/M33_model.image.tar.gz ${TMPDIR}/proc
-echo "Done M33_model"
-ls -al ${TMPDIR}/proc/
+    cp ${TMPDIR}/vos/Arecibo/M33_model.image.tar.gz ${TMPDIR}/proc
+    echo "Done M33_model"
+    ls -al ${TMPDIR}/proc/
 
-mkdir -m 777 ${TMPDIR}/proc/M33_b_c.ms/
-cp -R ${TMPDIR}/vos/M33_b_c.ms/* ${TMPDIR}/proc/M33_b_c.ms/
-echo "Done MS Set"
+    mkdir -m 777 ${TMPDIR}/proc/M33_b_c.ms/
+    cp -R ${TMPDIR}/vos/VLA/archival/M33_b_c.ms/* ${TMPDIR}/proc/M33_b_c.ms/
+    echo "Done MS Set"
+
+else
+    cp ${TMPDIR}/vos/M33_mask.image.zip ${TMPDIR}/proc
+    echo "Done M33_mask"
+    ls -al ${TMPDIR}/proc/
+
+    cp ${TMPDIR}/vos/M33_model.image.tar.gz ${TMPDIR}/proc
+    echo "Done M33_model"
+    ls -al ${TMPDIR}/proc/
+
+    mkdir -m 777 ${TMPDIR}/proc/M33_b_c.ms/
+    cp -R ${TMPDIR}/vos/M33_b_c.ms/* ${TMPDIR}/proc/M33_b_c.ms/
+    echo "Done MS Set"
+fi
+
 ls -al ${TMPDIR}/proc/
 ls -al ${TMPDIR}/proc/M33_b_c.ms/
 
 # Unmount
-sudo fusermount -u ${TMPDIR}/vos
+if [ $use_vos==true ]
+    then
+    fusermount -u ${TMPDIR}/vos
+else
+    sudo fusermount -u ${TMPDIR}/vos
+fi
 
 cd ${TMPDIR}/proc
 
