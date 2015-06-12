a53a57443572226ae6582f66471fab89ff19996c
e-koch <koch.eric.w@gmail.com>
2015-06-12 15:50:31 -0600

    Submitter script

---
+++ img_pipe/archival_data/submittus_prime.bash
@@ -0,0 +1,6 @@
+
+# Submit all of the submission files!
+
+for (( i = 10; i < 205; i++ )); do
+    canfar_submit /home/ekoch/code_repos/canfar_scripts/img_pipe/archival_data/single_channel_subs/single_channel_${i}.sub ewk_casa_4_20 126e8ef0-b816-43ed-bd5f-b1d4e16fdda0
+done
