2a7b9812c514501051495d49e0706ef00f3d1ff6
e-koch <koch.eric.w@gmail.com>
2015-06-12 15:49:59 -0600

    Alter generator to make individual sub files

--- img_pipe/archival_data/generate_singlechannel_sub.bash
+++ img_pipe/archival_data/generate_singlechannel_sub.bash
@@ -1,10 +1,20 @@
 
 # Creates the submission file for single channel cleaning.
 
-filename=${1}
+# filename=${1}
 
-cat run_single_channel_clean_header.txt > ${1}
+# cat run_single_channel_clean_header.txt > ${1}
 
-for (( i = 10; i < 215; i++ )); do
-    sed -e "s;%ARG%;$i;g" run_single_channel_clean_template.txt >> ${1}
+# for (( i = 90; i < 100; i++ )); do
+#     sed -e "s;%ARG%;$i;g" run_single_channel_clean_template.txt >> ${1}
+# done
+
+# Creates separate submission files for each channel
+# The other method was causing weird issues that don't seem easy to fix
+
+file_prefix=${1}
+
+for (( i = 10; i < 205; i++ )); do
+    cat run_single_channel_clean_header.txt > ${1}_channel_${i}.sub
+    sed -e "s;%ARG%;$i;g" run_single_channel_clean_template.txt >> ${1}_channel_${i}.sub
 done
