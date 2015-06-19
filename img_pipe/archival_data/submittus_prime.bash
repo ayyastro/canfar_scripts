ceba3797b119da48528236394b07fad153a15d13
e-koch <koch.eric.w@gmail.com>
2015-06-19 15:46:53 -0600

    Use the new snapshot

--- img_pipe/archival_data/submittus_prime.bash
+++ img_pipe/archival_data/submittus_prime.bash
@@ -29,7 +29,7 @@
 # done
 
 for ((i = 90; i < 92; i++)) do
-    canfar_submit /home/ekoch/code_repos/canfar_scripts/img_pipe/archival_data/single_channel_subs/single_channel_${i}.sub ewk_casa_6_17 126e8ef0-b816-43ed-bd5f-b1d4e16fdda0
+    canfar_submit /home/ekoch/code_repos/canfar_scripts/img_pipe/archival_data/single_channel_subs/single_channel_${i}.sub ewk_casa_6_19 126e8ef0-b816-43ed-bd5f-b1d4e16fdda0
 done
 
 running=$(seq 90 92)
@@ -41,9 +41,9 @@ while [[ $ii -geq 0 ]]; do
     for chan in $running; do
         if [[ -s output_files/output/single_channel_clean_${chan}.out ]]; then
             :
-        else
+        else]
             next_chan=$($left[0])
-            canfar_submit /home/ekoch/code_repos/canfar_scripts/img_pipe/archival_data/single_channel_subs/single_channel_${next_chan}.sub ewk_casa_6_17 126e8ef0-b816-43ed-bd5f-b1d4e16fdda0
+            canfar_submit /home/ekoch/code_repos/canfar_scripts/img_pipe/archival_data/single_channel_subs/single_channel_${next_chan}.sub ewk_casa_6_19 126e8ef0-b816-43ed-bd5f-b1d4e16fdda0
             running=( "${running[@]/$chan}" )
             left=( "${left[@]/$next_chan}" )
         fi
