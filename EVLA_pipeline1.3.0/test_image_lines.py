299d42fe6d4af971580ff3a74168f1b8279bd187
e-koch <koch.eric.w@gmail.com>
2015-06-23 10:55:11 -0600

    Add source imaging

--- EVLA_pipeline1.3.0/test_image_lines.py
+++ EVLA_pipeline1.3.0/test_image_lines.py
@@ -3,6 +3,9 @@
 Create test images of the central channels using the calibrator.
 '''
 
+import sys
+import os
+
 pipepath = '/lustre/aoc/observers/nm-7669/canfar_scripts/EVLA_pipeline1.3.0/'
 
 execfile(pipepath+"EVLA_pipe_restore.py")
@@ -11,38 +14,98 @@ execfile(pipepath+"EVLA_pipe_restore.py")
 myimsize = 2048
 mycell = "3arcsec"
 
-for i in spws:
-    for cal_field in calibrator_field_list:
-        print("Imaging calibrator in SPW "+str(i))
+try:
+    ms_name = sys.argv[1]
+    what_to_image = sys.argv[2]
+except IndexError:
+    ms_name = raw_input("MS Name? : ")
+    what_to_image = raw_input("Image cals, sources or both? : ")
 
+if what_to_image != "cals" or what_to_image != "sources" or what_to_image != "both":
+    raise TypeError("what_to_image must be 'cals', 'sources', or 'both'.")
+
+if not os.path.exists("test_images"):
+    os.mkdir("test_images")
+
+# Get the number of channels in each SPW
+tb.open(ms_name + "/SPECTRAL_WINDOW")
+nchans = tb.getcol("NUM_CHAN")
+ref_freqs = tb.getcol('REF_FREQUENCY')
+tb.close()
+
+for i in spws:
+    if what_to_image == "cals" or what_to_image == "both":
+        for cal_field in calibrator_field_list:
+            print("Imaging calibrator in SPW "+str(i))
+
+            try:
+                imname = "test_images/calibrator_field" + str(cal_field) + \
+                    "_spw" + str(i)
+
+                default('clean')
+                vis = 'calibrators.ms'
+                imagename = imname
+                outlierfile = ''
+                field = str(cal_field)
+                spw = str(i)
+                selectdata = False
+                mode = 'channel'
+                nchan = 1
+                width = 1
+                start = 0
+                reffreq = ''
+                gridmode = ''
+                niter = 0
+                threshold = '0.0mJy'
+                imagermode = ''
+                imsize = [myimsize, myimsize]
+                cell = [mycell, mycell]
+                phasecenter = ''
+                restfreq = ''
+                stokes = 'I'
+                weighting = 'uniform'
+                uvtaper = False
+                modelimage = ''
+                restoringbeam = ['']
+                pbcor = False
+                minpb = 0.2
+                calready = False
+                allowchunk = False
+                async = False
+                clean()
+
+            except Exception, e:
+                logprint("Problem with " + str(cal_field) + " " + str(i) + " " + imname)
+                print e
+
+    # Now image the central channels of the source
+    if what_to_image == "sources" or what_to_image == "both":
         try:
-            imname = "calibrator_field" + str(cal_field) + "_spw" + str(i)
+            imname = "test_images/source_field" + "_spw" + str(i)
 
             default('clean')
-            vis = 'calibrators.ms'
+            vis = ms_name
             imagename = imname
             outlierfile = ''
-            field = str(cal_field)
+            field = "M33*"
             spw = str(i)
             selectdata = False
             mode = 'channel'
             nchan = 1
             width = 1
-            start = 0
+            start = nchans[i] / 2
             reffreq = ''
             gridmode = ''
             niter = 0
             threshold = '0.0mJy'
-            imagermode = ''
+            imagermode = 'mosaic'
             imsize = [myimsize, myimsize]
             cell = [mycell, mycell]
-            phasecenter = ''
-            restfreq = ''
+            phasecenter = 'J2000 01h33m50.904 +30d39m35.79'
+            restfreq = str(restfreq)+"Hz"
             stokes = 'I'
-            weighting = 'uniform'
+            weighting = 'natural'
             uvtaper = False
-            modelimage = ''
-            restoringbeam = ['']
             pbcor = False
             minpb = 0.2
             calready = False
@@ -51,5 +114,5 @@ for i in spws:
             clean()
 
         except Exception, e:
-            logprint("Problem with " + str(cal_field) + " " + str(i) + " " + imname)
+            logprint("Problem with " + str(i) + " " + imname)
             print e
