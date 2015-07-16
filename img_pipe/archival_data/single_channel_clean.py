048dcc39703cb00ce5616e9ed2b07be8dbde4038
e-koch <koch.eric.w@gmail.com>
2015-07-15 21:34:58 -0600

    Remove the largest scale; causes severe artifacts in some channels

--- img_pipe/archival_data/single_channel_clean.py
+++ img_pipe/archival_data/single_channel_clean.py
@@ -14,7 +14,7 @@ out_root = vis[:-3]
 clean(vis=vis, imagename=out_root+'.clean', field='M33*',
       restfreq='1420.40575177MHz',
       mode='channel', width=1, nchan=1, start=1,
-      cell='1.5arcsec', multiscale=[0, 4, 8, 20, 40, 80],
+      cell='1.5arcsec', multiscale=[0, 4, 8, 20, 40],
       threshold='2.2mJy/beam', imagermode='mosaic', gain=0.5,
       imsize=[4096, 4096], weighting='natural', robust=0.0, niter=50000,
       pbcor=True, minpb=0.7, interpolation='linear', usescratch=True,
